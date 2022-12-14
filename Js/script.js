d348765b60913055bacc03d460916e19594addda
Paolo224 <picariellopaolo224@gmail.com>
2022-12-14 15:54:37 +0100

    Prima Milestone completa

--- Js/script.js
+++ Js/script.js
@@ -3,7 +3,48 @@ const { crateApp } = Vue
 .createApp({
     data(){
         return {
-            
+            list : [
+                {
+                    text : 'pane',
+                    done : false,
+                },
+                {
+                    text : 'latte',
+                    done : false,
+                },
+                {
+                    text : 'pasta',
+                    done : true,
+                },
+                {
+                    text : 'carne',
+                    done : true,
+                },
+                {
+                    text : 'uova',
+                    done : true,
+                },
+                {
+                    text : 'yogurt',
+                    done : true,
+                },
+                {
+                    text : 'olio',
+                    done : false,
+                },
+                {
+                    text : 'fagioli',
+                    done : true,
+                },
+                {
+                    text : 'aceto',
+                    done : false,
+                },
+                {
+                    text : 'sale',
+                    done : false,
+                },
+            ],
         }
     },
 
